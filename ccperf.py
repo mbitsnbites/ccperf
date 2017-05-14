84fdeff919dcc284a38b5bee53acf68720a45819
m <m@bitsnbites.eu>
2017-05-14 12:09:39 +0200

    Use proper temp-file names

--- ccperf.py
+++ ccperf.py
@@ -6,6 +6,7 @@ import re
 import shlex
 import sys
 import subprocess
+import tempfile
 
 
 def count_lines(file_name):
@@ -16,42 +17,50 @@ def count_lines(file_name):
     return count
 
 
-def gcc_get_preprocessed_size(cmd, dir):
-    # TODO(m): Use a unique temporary file name!
-    cpp_file = '/tmp/ccperf-1234.i'
-
-    opts = shlex.split(cmd)
-    for i in range(len(opts) - 1, 0, -1):
-        opt = opts[i].strip()
-        if opt == '':
-            # Drop empty items.
-            opts.pop(i)
-        if opt == '-c':
-            # We only do the preprocessing step.
-            opts[i] = '-E'
-        elif opt == '-o' and (i + 1) < len(opts):
-            # Output to a temporary file.
-            opts[i + 1] = cpp_file
-        elif opt in ['-M', '-MM', '-MG', '-MP', '-MD', '-MMD']:
-            # Drop dependency generation.
-            opts.pop(i)
-        elif opt in ['-MF', '-MT', '-MQ'] and (i + 1) < len(opts):
-            # Drop dependency generation.
-            opts.pop(i + 1)
-            opts.pop(i)
-
+def gcc_preprocess_file(cmd, dir):
+    # Create a temporary file.
+    temp_fd, cpp_file = tempfile.mkstemp('.i', 'ccperf')
     try:
-        subprocess.check_call(opts, stderr=subprocess.STDOUT, cwd=dir)
-        size = os.stat(cpp_file).st_size
-        return { 'bytes': size, 'lines': count_lines(cpp_file) }
-    except:
-        print 'FAIL: Unable to get preprocessed size.'
-        return { 'bytes': 0, 'lines': 0 }
+        opts = shlex.split(cmd)
+        for i in range(len(opts) - 1, 0, -1):
+            opt = opts[i].strip()
+            if opt == '':
+                # Drop empty items.
+                opts.pop(i)
+            if opt == '-c':
+                # We only do the preprocessing step.
+                opts[i] = '-E'
+            elif opt == '-o' and (i + 1) < len(opts):
+                # Output to a temporary file.
+                opts[i + 1] = cpp_file
+            elif opt in ['-M', '-MM', '-MG', '-MP', '-MD', '-MMD']:
+                # Drop dependency generation.
+                opts.pop(i)
+            elif opt in ['-MF', '-MT', '-MQ'] and (i + 1) < len(opts):
+                # Drop dependency generation.
+                opts.pop(i + 1)
+                opts.pop(i)
 
+        try:
+            subprocess.check_output(opts, stderr=subprocess.STDOUT, cwd=dir)
+            size = os.stat(cpp_file).st_size
+            num_lines = count_lines(cpp_file)
+            result = { 'bytes': size, 'lines': num_lines }
+        except:
+            print '*** Preprocessing the source file failed.'
+            result = { 'bytes': 0, 'lines': 0 }
 
-def get_preprocessed_size(cmd, dir):
+    finally:
+        # We're done with the temporary file.
+        os.close(temp_fd)
+        if os.path.isfile(cpp_file):
+            os.remove(cpp_file)
+
+    return result
+
+def preprocess_file(cmd, dir):
     # TODO(m): Here we assume that we're running a compiler that understands GCC options.
-    return gcc_get_preprocessed_size(cmd, dir)
+    return gcc_preprocess_file(cmd, dir)
 
 
 def get_original_size(src_file):
@@ -143,7 +152,7 @@ def record():
         size = get_original_size(src_file)
 
         # Get the preprocessed size for this source file.
-        size_pp = get_preprocessed_size(item['command'], dir)
+        size_pp = preprocess_file(item['command'], dir)
 
         record_db[src_file] = {'headers_all': headers_all, 'headers_sys': headers_sys, 'size': size['bytes'],
                                'size_pp': size_pp['bytes'], 'lines': size['lines'], 'lines_pp': size_pp['lines']}
