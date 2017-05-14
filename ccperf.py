c99ba1f2a8e0b0947cccaadcedb36cf2b6010f09
m <m@bitsnbites.eu>
2017-05-14 12:10:14 +0200

    Collect metrics in a single pass (2x speed)

--- ccperf.py
+++ ccperf.py
@@ -41,14 +41,32 @@ def gcc_preprocess_file(cmd, dir):
                 opts.pop(i + 1)
                 opts.pop(i)
 
+        # Add the -H option to output all included header files.
+        opts.append('-H')
+
         try:
-            subprocess.check_output(opts, stderr=subprocess.STDOUT, cwd=dir)
+            res = subprocess.check_output(opts, stderr=subprocess.STDOUT, cwd=dir)
+
+            # Get a list of all included header files.
+            header_files = []
+            lines = res.split('\n')
+            re_prg = re.compile('^\.+ ')
+            for line in lines:
+                if re_prg.match(line):
+                    file_name = line[(line.index(' ') + 1):].strip()
+                    if not os.path.isabs(file_name):
+                        file_name = os.path.abspath(os.path.join(dir, file_name))
+                    header_files.append(file_name)
+            header_files = list(set(header_files))
+
+            # Get the size of the preprocessed file.
             size = os.stat(cpp_file).st_size
             num_lines = count_lines(cpp_file)
-            result = { 'bytes': size, 'lines': num_lines }
+
+            result = { 'bytes': size, 'lines': num_lines, 'header_files': header_files }
         except:
             print '*** Preprocessing the source file failed.'
-            result = { 'bytes': 0, 'lines': 0 }
+            result = { 'bytes': 0, 'lines': 0, 'header_files': [] }
 
     finally:
         # We're done with the temporary file.
@@ -65,52 +83,6 @@ def preprocess_file(cmd, dir):
 
 def get_original_size(src_file):
     return { 'bytes': os.stat(src_file).st_size, 'lines': count_lines(src_file) }
-
-
-def gcc_get_included_files(cmd, dir):
-    opts = shlex.split(cmd)
-    for i in range(len(opts) - 1, 0, -1):
-        opt = opts[i].strip()
-        if opt == '':
-            # Drop empty items.
-            opts.pop(i)
-        if opt == '-c':
-            # We only do the preprocessing step.
-            opts[i] = '-E'
-        elif opt == '-o' and (i + 1) < len(opts):
-            # Do not ouptut anything.
-            # TODO(m): Support Windows.
-            opts[i + 1] = '/dev/null'
-        elif opt in ['-M', '-MM', '-MG', '-MP', '-MD', '-MMD']:
-            # Drop dependency generation.
-            opts.pop(i)
-        elif opt in ['-MF', '-MT', '-MQ'] and (i + 1) < len(opts):
-            # Drop dependency generation.
-            opts.pop(i + 1)
-            opts.pop(i)
-    # Add the -H option to output all included header files.
-    opts.append('-H')
-
-    try:
-        res = subprocess.check_output(opts, stderr=subprocess.STDOUT, cwd=dir)
-        files = []
-        lines = res.split('\n')
-        re_prg = re.compile('^\.+ ')
-        for line in lines:
-            if re_prg.match(line):
-                file_name = line[(line.index(' ') + 1):].strip()
-                if not os.path.isabs(file_name):
-                    file_name = os.path.abspath(os.path.join(dir, file_name))
-                files.append(file_name)
-        return list(set(files))
-    except:
-        print 'FAIL: Unable to query number of included header files.'
-        return []
-
-
-def collect_header_files(cmd, dir):
-    # TODO(m): Here we assume that we're running a compiler that understands GCC options.
-    return gcc_get_included_files(cmd, dir)
 
 
 def is_system_header(file_name):
@@ -139,20 +111,19 @@ def record():
         dir = item['directory']
         src_file = os.path.abspath(os.path.join(dir, item['file']))
 
-        # Count header files for this source file.
-        header_files = collect_header_files(item['command'], dir)
-        headers_all = 0
-        headers_sys = 0
-        for header_file in header_files:
-            headers_all += 1
-            if is_system_header(header_file):
-                headers_sys += 1
-
         # Get the original size for this file.
         size = get_original_size(src_file)
 
-        # Get the preprocessed size for this source file.
+        # Run the preprocessor to collect size metrics.
         size_pp = preprocess_file(item['command'], dir)
+
+        # Count header files for this source file.
+        headers_all = 0
+        headers_sys = 0
+        for header_file in size_pp['header_files']:
+            headers_all += 1
+            if is_system_header(header_file):
+                headers_sys += 1
 
         record_db[src_file] = {'headers_all': headers_all, 'headers_sys': headers_sys, 'size': size['bytes'],
                                'size_pp': size_pp['bytes'], 'lines': size['lines'], 'lines_pp': size_pp['lines']}
@@ -161,6 +132,10 @@ def record():
     record_db_file = os.path.join(build_dir, '.ccperf')
     with open(record_db_file, 'w') as file:
         json.dump(record_db, file, sort_keys=True, indent=2, separators=(',', ': '))
+
+def report():
+    # TODO(m): Implement me!
+    return
 
 def main():
     parser = argparse.ArgumentParser(description='Generate IDE projects from Meson.')
